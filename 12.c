f6b43a64c454ff900a648a47214e756f921b0940
ramanip <31653594+ramanip@users.noreply.github.com>
2018-01-10 10:54:00 +0000

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char ch;
+printf("enter the alphabet ");
+scanf("%c,&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u' || ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U');
+{
+printf("\n %c a is vowel.",ch);
+}
+else
+{
+printf("\n %c a is constant.",ch);
+}
