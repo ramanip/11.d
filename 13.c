47316d46898e527139797b788e1e7e0997d9601c
ramanip <31653594+ramanip@users.noreply.github.com>
2018-01-10 11:10:29 +0000

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the a value");
+scanf("%d",&a);
+printf("enter the  b value");
+scanf("%d",&b);
+printf("enter the c value");
+scanf("%d",&c);
+if(a>b&& a>c);
+a=largest number;
+else
+{
+if(b>c&& b>c);
+b=largest number;
+}
+else
+{
+if(c>a&& c>b);
+c=largest number;
+}
