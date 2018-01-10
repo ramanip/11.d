e1ca475ade3349c03cc606bc8b89de5c9de7b08e
ramanip <31653594+ramanip@users.noreply.github.com>
2018-01-10 11:28:11 +0000

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,16 @@
+#inclde<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the value n")
+scanf("%d",&n);
+if(n%4=0)
+{
+printf("leap year");
+}
+else
+{
+printf("not leap year");
+}
+}
