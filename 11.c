744972e6e33d263a15e47220d0cf57aa78d29f04
ramanip <31653594+ramanip@users.noreply.github.com>
2018-01-10 10:34:40 +0000

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+#include<conio.h>
+int main()
+{
+char ch;
+printf("enter anyb character:");
+scanf("%c",&c);
+if(ch>='a' && ch<='z' || (ch>='A' && ch<='Z'))
+{
+printf("character is alphabet");
+else
+{
+printf("character is not alphabet");
+}
+return 0
+}
