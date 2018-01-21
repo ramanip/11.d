d6818c3673c871ccdb2ecf4db7cd8679d5a47ac9
ramanip <31653594+ramanip@users.noreply.github.com>
2018-01-21 13:47:39 +0530

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+ int n, reversedInteger = 0;
+ printf("Enter an integer")
+ scanf("%d", &n);
+ originalInteger = n; 
+ while( n!=0 )
+ {
+ remainder = n%1
+ n/=10;
+ }
+ if (originalInteger == reversedInteger)
+ printf("%d is a palindrome.", originalInteger);
+ else
+ printf("%d is not a palindrome.", originalInteger);
+ }
