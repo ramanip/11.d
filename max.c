a667c4f1b468a4cd7c90726bb151d7fc8c1a4b8d
ramanip <31653594+ramanip@users.noreply.github.com>
2018-01-24 16:24:06 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10];
+for(i=0;i<=10;i++);
+{
+scanf("%d",a[10])
+printf("%d",a[10])
+}
+max a[0]
+for(i=0;i<=10;1++);
+{
+if(a[i]>max)
+max=a[i];
+}
+printf("%d",max);
+}
